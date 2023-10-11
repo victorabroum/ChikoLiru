4fee09bf73cb4bca5abaad4d1fcf427856aafdab
Victor Vasconcelos <vfvasconcelos3010@gmail.com>
2023-10-11 16:57:36 -0400

    Add new project

---
+++ Chiko Liru/Chiko Liru/Chiko_LiruApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chiko_LiruApp.swift
+//  Chiko Liru
+//
+//  Created by Victor Vasconcelos on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Chiko_LiruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
